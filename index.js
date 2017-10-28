89c63c45e3a43be1227e65cfb75b84bca147dbe2
wobusimanong <269410834@qq.com>
2017-10-28 16:27:22 -0400

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const path = require('path');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
@@ -12,8 +13,10 @@ mongoose.connect(config.uri, (err) => {
     }
 });
 
+app.use(express.static(__dirname + 'client/dist/'));
+
 app.get('*', (req, res) => {
-    res.send('<h1>hello, world</h1>');
+    res.sendFile(path.join(__dirname + /client/dist/index.html));
 });
 
 app.listen(8080, () => {
